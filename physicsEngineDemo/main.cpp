6c4b70f7b0dc64e74e787513f48f0e4461b71291
AldenHiggins <johnaldenhiggins@gmail.com>
2015-02-12 00:22:12 -0600

    Hopefully opengl is set up with glfw/glew

--- physicsEngineDemo/main.cpp
+++ physicsEngineDemo/main.cpp
@@ -1,290 +1,58 @@
-#include <iostream>
-#include <conio.h>
-#include <vector>
-#include <GL/glut.h>
-#include "particle.h"
-#include "Drawing.h"
-#include <windows.h>
-
-// Include the OculusVR SDK
-#include "OVR_CAPI.h"
-#include "Kernel\OVR_Math.h"
-
-using namespace OVR;
-using namespace std;
-
-// Choose whether the SDK performs rendering/distortion, or the application. 
-#define          SDK_RENDER 1  //Do NOT switch until you have viewed and understood the Health and Safety message.
-//Disabling this makes it a non-compliant app, and not suitable for demonstration. In place for development only.
-const bool       FullScreen = true; //Should be true for correct timing.  Use false for debug only.
+#include <GLFW/glfw3.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 
-
-
-void moveCamera()
+static void error_callback(int error, const char* description)
 {
-	// do mouse calculations
-	float camMoveFraction = 0.3f;
-	POINT cursor;
-
-	GetCursorPos(&cursor);
-
-	if (cursor.x > xCenter + 1)
-	{
-		cameraRotation[2] += 1;
-	}
-	else if (cursor.x < xCenter - 1)
-	{
-		cameraRotation[2] -= 1;
-	}
-
-	if (cursor.y > yCenter + 1)
-	{
-		cameraRotation[0] += 1;
-	}
-	else if (cursor.y < yCenter - 1)
-	{
-		cameraRotation[0] -= 1;
-	}
-
-	SetCursorPos(initialWindowX + (.5 * windowSizeX), initialWindowY + (.5 * windowSizeY));
-
-	std::cout << "cameraRotation[2]: " << cameraRotation[2] << "\n";
-	std::cout << "cameraRotation[0]: " << cameraRotation[0] << "\n";
-
-	glRotatef(cameraRotation[0], 1, 0.0, 0.0);
-	glRotatef(cameraRotation[2], 0.0, 0.0, 1.0);
-	glTranslatef(-cameraX, -cameraY, -cameraZ);
+	fputs(description, stderr);
 }
-
-void display()
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
-
-
-
-	
-	//glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	//glMatrixMode(GL_PROJECTION);
-	moveCamera();
-
-	glMatrixMode(GL_MODELVIEW);
-	//glTranslatef(cameraX, cameraY, cameraZ);
-	
-	//glRotatef(cameraRotation[0], cameraX + 1, cameraY, cameraZ);
-	//glRotatef(cameraRotation[1], cameraX, cameraY + 1, cameraZ);
-	//glRotatef(cameraRotation[2], cameraX, cameraY, cameraZ + 5);
-	//glRotatef(cameraRotation[1], 0.0, cameraY - 32, 0.0);
-	//glRotatef(cameraRotation[2], 0.0, 0.0, cameraZ);
-	
-	//glMatrixMode(GL_MODELVIEW);
-	//glLoadIdentity();
-	//
-	//glEnable(GL_LIGHTING);
-	//glEnable(GL_LIGHT0);
-	//glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-	//glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, light_direction);
-	//glLightfv(GL_LIGHT1, GL_POSITION, light_position);
-	//glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, light_direction2);
-
-
-
-	//glEnable(GL_LIGHTING);
-	
-	//glLoadIdentity();
-	//std::cout << particles.size() << "\n";
-	for (int i = 0; i < particles.size(); ++i)
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+int main(void)
+{
+	GLFWwindow* window;
+	glfwSetErrorCallback(error_callback);
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
+	window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+	if (!window)
 	{
-		particles[i].draw();
+		glfwTerminate();
+		exit(EXIT_FAILURE);
 	}
-	//drawLight();
-	drawGround();
-	
-
-	glutSwapBuffers();
-	//glFlush();
-}
-
-void idle()
-{
-	glutPostRedisplay();
-}
-
-void specialFunc(int key, int x, int y)
-{
-	switch (key)
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+	glfwSetKeyCallback(window, key_callback);
+	while (!glfwWindowShouldClose(window))
 	{
-	case GLUT_KEY_UP:
-		translateY += .1;
-		break;
-	case GLUT_KEY_RIGHT:
-		translateX -= .1;
-		break;
-	case GLUT_KEY_LEFT:
-		translateX += .1;
-		break;
-	case GLUT_KEY_DOWN:
-		translateY -= .1;
-		break;
+		float ratio;
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+		ratio = width / (float)height;
+		glViewport(0, 0, width, height);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		glRotatef((float)glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+		glBegin(GL_TRIANGLES);
+		glColor3f(1.f, 0.f, 0.f);
+		glVertex3f(-0.6f, -0.4f, 0.f);
+		glColor3f(0.f, 1.f, 0.f);
+		glVertex3f(0.6f, -0.4f, 0.f);
+		glColor3f(0.f, 0.f, 1.f);
+		glVertex3f(0.f, 0.6f, 0.f);
+		glEnd();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
-}
-
-void keyFunc(unsigned char key, int x, int y)
-{
-	switch (key)
-	{
-	case 'w':
-		cameraY += .1;
-		break;
-	case 's':
-		cameraY -= .1;
-		break;
-	case 'a':
-		cameraX -= .1;
-		break;
-	case 'd':
-		cameraX += .1;
-		break;
-	case 'q':
-		cameraZ -= .1;
-		break;
-	case 'e':
-		cameraZ += .1;
-		break;
-	// Exit by pressing 'esc'
-	case 27:
-		exit(0);
-		break;
-	}
-}
-//SetCursorPos(initialWindowX + (.5 * windowSizeX)
-
-
-void mouseMoveFunction(int x, int y)
-{
-	std::cout << "X: " << x << "\n";
-	std::cout << "Y: " << y << "\n";
-	std::cout << "XCenter: " << xCenter << "\n";
-	std::cout << "YCenter: " << yCenter << "\n";
-	if (x > xCenter + 20)
-	{
-		cameraRotation[2] += 1;
-	}
-	else if (x < xCenter - 20)
-	{
-		cameraRotation[2] -= 1;
-	}
-
-	//if (y > yCenter)
-	//{
-	//	cameraRotation[2] -= .1;
-	//}
-	//else if (y < yCenter)
-	//{
-	//	cameraRotation[2] += .1;
-	//}
-
-	//xCenter = initialWindowX + (.5 * windowSizeX);
-	//yCenter = initialWindowY + (.5 * windowSizeY);
-	/*previousX = x;
-	previousY = y;*/
-}
-
-
-void Init(ovrHmd &hmd)
-{
-	ovr_Initialize();
-	hmd = ovrHmd_Create(0);
-	if (!hmd) return;
-
-	//ovrHmd_ConfigureTracking(hmd, 1, 0);
-	ovrHmd_ConfigureTracking(hmd, ovrTrackingCap_Orientation | ovrTrackingCap_Position, 0);
-}
-
-
-void Output(const ovrHmd &hmd)
-{
-	// Optional: we can overwrite the previous console to more
-	// easily see the values change
-	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
-	CONSOLE_SCREEN_BUFFER_INFO bufferInfo;
-	GetConsoleScreenBufferInfo(h, &bufferInfo);
-
-	ovrFrameTiming frameTiming;
-
-	while (hmd) {
-		frameTiming = ovrHmd_BeginFrameTiming(hmd, 0);
-		ovrTrackingState ts = ovrHmd_GetTrackingState(hmd, frameTiming.ScanoutMidpointSeconds);
-
-		if (ts.StatusFlags & (ovrStatus_OrientationTracked | ovrStatus_PositionTracked))
-		{
-			// The cpp compatibility layer is used to convert ovrPosef to Posef (see OVR_Math.h)
-			Posef pose = ts.HeadPose.ThePose;
-			float yaw, pitch, roll;
-			pose.Rotation.GetEulerAngles<Axis_Y, Axis_X, Axis_Z>(&yaw, &pitch, &roll);
-
-			// Optional: move cursor back to starting position and print values
-			SetConsoleCursorPosition(h, bufferInfo.dwCursorPosition);
-			cout << "yaw: " << RadToDegree(yaw) << endl;
-			cout << "pitch: " << RadToDegree(pitch) << endl;
-			cout << "roll: " << RadToDegree(roll) << endl;
-
-			float x, y, z;
-			x = pose.Translation.x;
-			y = pose.Translation.y;
-			z = pose.Translation.z;
-			cout << endl;
-
-			cout << "x: " << x << endl;
-			cout << "y: " << y << endl;
-			cout << "z: " << z << endl;
-			Sleep(50);
-
-			ovrHmd_EndFrameTiming(hmd);
-
-			if (_kbhit()) exit(0);
-		}
-		else
-		{
-			// Make sure to endFrameTiming no matter what
-			ovrHmd_EndFrameTiming(hmd);
-		}
-	}
-}
-
-
-void Clear(const ovrHmd &hmd)
-{
-	ovrHmd_Destroy(hmd);
-	ovr_Shutdown();
-}
-
-
-int main(int argc, char **argv)
-{
-	// Oculus initialization code
-	ovrHmd hmd;
-	Init(hmd);
-	
-
-	//// OPENGL setup code
-	//glutInit(&argc, argv);
-	//glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	//glutInitWindowSize(windowSizeX, windowSizeY);
-	//glutInitWindowPosition(initialWindowX, initialWindowY);
-	//glutCreateWindow("Alden's Physics Demo");
-	//glutSpecialFunc(specialFunc);
-	////glutPassiveMotionFunc(mouseMoveFunction);
-	//glutKeyboardFunc(keyFunc);
-	//glutDisplayFunc(display);
-	//glutIdleFunc(idle);
-	//init();
-	//glutMainLoop();
-
-	Output(hmd);
-	// Clean up Oculus
-	Clear(hmd);
-	return 0;
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
 }
